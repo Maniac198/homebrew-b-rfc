bccd30d461ff36ebbdda8f5f7bdc0dfaf104afb3
mani <manish.choudhary@helpshift.com>
2026-05-19 13:35:57 +0530

    Update sha + url

--- Formula/b-rfc.rb
+++ Formula/b-rfc.rb
@@ -1,8 +1,8 @@
 class BRfc < Formula
   desc "RFC helper script"
   homepage "https://github.com/maniac198/b-rfc"
-  url "https://github.com/maniac198/b-rfc/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "353600ad9dae8d2bcde49240064184713a409f02d27a442dd53ee348686fd383"
+  url "https://github.com/maniac198/b-rfc/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "3802ba7fae7e2850bb9000a78e3f1f722a002df0dae4d37e2c05621cce075d15"
   license "MIT"
 
   def install
